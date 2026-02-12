f4f7f7df25ae40136b09050e57b1996e5ab40e9d
Fede_C3N <fede.ramirez.t@gmail.com>
2026-02-12 12:11:45 +0100

    Add parameters to config file Create first forward model based on template Function to plot sources positions

--- src/sEEGnal/sources_reconstruction/forward.py
+++ src/sEEGnal/sources_reconstruction/forward.py
@@ -8,6 +8,95 @@ Federico Ramírez-Toraño
 
 """
 
+# Import
+import mne
+
+from sEEGnal.tools.mne_tools import prepare_eeg
+
+
 def make_forward_model(config,bids_path):
 
-    print('  It runs')
+    # Estimate the parameters of the forward model
+    if config['forward']['use_template']:
+        forward_model = template_forward_model(config,bids_path)
+    else:
+        forward_model = []
+
+
+
+    return forward_model
+
+
+def template_forward_model(config, bids_path):
+    """
+
+    Use the Freesurfer template to create a forward model
+
+    """
+
+    # Load the clean EEG
+    sobi = {
+        'desc': 'sobi',
+        'components_to_include': ['brain', 'other'],
+        'components_to_exclude': []
+    }
+    freq_limits = [
+        config['component_estimation']['low_freq'],
+        config['component_estimation']['high_freq']
+    ]
+    crop_seconds = config['component_estimation']['crop_seconds']
+    resample_frequency = config['component_estimation']['resample_frequency']
+    channels_to_include = config['global']["channels_to_include"]
+    channels_to_exclude = config['global']["channels_to_exclude"]
+
+    # Load the clean data
+    raw = prepare_eeg(
+        config,
+        bids_path,
+        preload=True,
+        channels_to_include=channels_to_include,
+        channels_to_exclude=channels_to_exclude,
+        freq_limits=freq_limits,
+        notch_filter=True,
+        resample_frequency=resample_frequency,
+        set_annotations=True,
+        crop_seconds=crop_seconds,
+        rereference='average'
+    )
+
+    raw = prepare_eeg(
+        config,
+        bids_path,
+        raw=raw,
+        apply_sobi=sobi,
+        freq_limits=[2, 45],
+        metadata_badchannels=True,
+    )
+
+    # Get the FreeSurfer fsaverage information
+    fs_dir = mne.datasets.fetch_fsaverage(verbose=False)
+    subject = config['forward']['template']['subject']
+    trans = config['forward']['template']['trans']
+    bem = fs_dir / "bem" / config['forward']['template']['bem']
+
+    # Define our sources
+    src = mne.setup_volume_source_space(
+        subject=subject,
+        pos=config['forward']['template']['pos'],
+        mri=config['forward']['template']['mri'],
+        bem=None,
+        add_interpolator=True
+    )
+
+    # Create the output forward model
+    forward_model = mne.make_forward_solution(
+        raw.info,
+        trans,
+        src,
+        bem,
+        meg=False,
+        eeg=True,
+        mindist=5.0
+    )
+
+    return forward_model
---
+++ test/scripts/check_source_reconstruction/plot_forward_model.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Plot forward model
+
+Federico Ramírez-Toraño
+12/02/2026
+
+"""
+
+
+# Imports
+import os
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+import mne
+from test.init.init import init
+
+# Init the database
+config, _, _, _, _ = init()
+
+# Get the FreeSurfer fsaverage information
+fs_dir = mne.datasets.fetch_fsaverage(verbose=False)
+subject = config['forward']['template']['subject']
+trans = config['forward']['template']['trans']
+bem = fs_dir / "bem" / config['forward']['template']['bem']
+
+# fsaverage T1 MRI
+mri_file = os.path.join(fs_dir, 'mri', 'T1.mgz')
+
+# Load MRI
+mri = nib.load(mri_file)
+mri_data = mri.get_fdata()
+
+# Define our sources
+src = mne.setup_volume_source_space(
+    subject=subject,
+    pos=config['forward']['template']['pos'],
+    mri=config['forward']['template']['mri'],
+    bem=None,
+    add_interpolator=True
+)
+
+# Get the positions in mm and then voxels
+coords = src[0]['rr']
+coords_vox = nib.affines.apply_affine(np.linalg.inv(mri.affine), coords * 1000)  # metros -> mm
+coords_vox = np.round(coords_vox).astype(int)
+
+# Plot
+slices = np.unique(coords_vox[:,1])
+index = range(0,len(slices),4)
+slices = slices[index]
+
+fig, axes = plt.subplots(1, len(slices), figsize=(15,4))
+for i, y in enumerate(slices):
+    ax = axes[i]
+    slice_data = mri_data[:, y, :].T
+    ax.imshow(slice_data, cmap='gray', origin='lower')
+    mask_slice = coords_vox[:,1] == y
+    ax.scatter(coords_vox[mask_slice,0], coords_vox[mask_slice,2], s=5,
+               color='red',alpha=0.6)
+    ax.set_title(f'Coronal y={y}')
+    ax.axis('off')
+
+plt.tight_layout()
+plt.show(block=True)
